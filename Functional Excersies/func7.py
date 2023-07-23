cdffdb4ef3bbe495cd0599e81546210d9147a55e
Tejas Falke <tejasdf.77@gmail.com>
2023-07-23 09:00:59 +0530

    exs-er-size-with-function

--- Functional Excersies/func7.py
+++ Functional Excersies/func7.py
@@ -1,14 +1,15 @@
+'''
 subject = int(input("How Many subject you have : "))
 marks = []
 def student():
     for i in range(1,subject+1):
         m = int(input(f"Subject {i} :"))
         marks.append(m)
-    print(f"subject", marks)
+    print(f"subject {marks}")
 def grade():
     sum_of_marks = sum(marks)
     average = sum_of_marks/subject
-    print(average)
+    print('average',average)
     if average >= 100:
         print("Try with valid marks..")
     elif average >= 71 and average <= 100:
@@ -21,5 +22,33 @@ def grade():
         print("tRY next time...")
 
 
+student()
+grade()
+'''
+
+def student():
+    
+    print(f"Subject marks: {marks}")
+
+def grade():
+    try:
+        sum_of_marks = sum(marks)
+        average = sum_of_marks / subject
+        print(f"Average: {average}")
+        if average >= 100:
+            print("Try with valid marks..")
+        elif average >= 71 and average <= 100:
+            print("First Class")
+        elif average >= 61 and average <= 70:
+            print("Second Class")
+        elif average >= 35 and average <= 60:
+            print("Third Class")
+        else:
+            print("Try next time...")
+    except ZeroDivisionError:
+        print("Invalid input")
+
+subject = int(input("How many subjects do you have? "))
+marks = [int(input(f"Subject {i}: ")) for i in range(1, subject + 1)]
 student()
 grade()
